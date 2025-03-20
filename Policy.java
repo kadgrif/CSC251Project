4b00b533d85a1300d006cdfae66bb37edb8f97e8
kadgrif <kgriff60@email.cpcc.edu>
2025-03-19 22:59:54 -0400

    Added documentation comments

--- Policy.java
+++ Policy.java
@@ -47,6 +47,7 @@ public class Policy
       
       /**
          The getPolicyNumber method returns the value of the policyNumber variable.
+         @return The user's policy number.
       */
       public String getPolicyNumber()
          {
@@ -63,72 +64,119 @@ public class Policy
 
       /**
          The getProviderName method returns the value of the providerName variable.
+         @return The user's provider name.
       */
       public String getProviderName()
          {
             return providerName;
          }
-         
+
+      /**
+         The setFirstName method mutates the value of the firstName variable.
+      */
       public void setFirstName(String pFirstName)
          {
             firstName = pFirstName;
          } 
-      
+
+      /**
+         The getFirstName method returns the value of the firstName variable.
+         @return The user's first name.
+      */
       public String getFirstName()
          {
             return firstName;
          }
-         
+
+      /**
+         The setLastName method mutates the value of the lastName variable.
+      */
       public void setLastName(String pLastName)
          {
             lastName = pLastName;
          } 
-      
+
+      /**
+         The getLastName method returns the value of the lastName variable.
+         @return The user's last name.
+      */
       public String getLastName()
          {
             return lastName;
          }
-         
+      
+      /**
+         The setSmokingStatus method mutates the value of the smokingStatus variable.
+      */   
       public void setSmokingStatus(String pSmokingStatus)
          {
             smokingStatus = pSmokingStatus;
          }   
-      
+
+      /**
+         The getSmokingStatus method returns the value of the smokingStatus variable.
+         @return The smoking status of the user.
+      */
       public String getSmokingStatus()
          {
             return smokingStatus;
          }
-         
+
+      /**
+         The setAge method mutates the value of the age variable.
+      */
       public void setAge(int pAge)
          {
             age = pAge;
          }    
-      
+
+      /**
+         The getAge method returns the value of the age variable.
+         @return The age of the user.
+      */
       public int getAge()
          {
             return age;
          }  
-         
+
+      /**
+         The setHeight method mutates the value of the height variable.
+      */
       public void setHeight(int pHeight)
          {
             height = pHeight;
          }     
-      
+
+      /**
+         The getHeight method return the value of the height variable.
+         @return The height of the user.
+      */
       public double getHeight()
          {
             return height;
          }
-         
+
+      /**
+         The setWeight method mutates the value of the weight variable.
+      */
       public void setWeight(int pWeight)
          {
             weight = pWeight;
          } 
-         
+
+      /**
+         The getWeight method returns the value of the weight variable.
+         @return The weight of the user.
+      */
       public double getWeight()
          {
             return weight;
          }
-         
+
+      /**
+         The calculateBMI method returns the value of the BMI variable.
+         @return The calculation of Body Mass Index based of user age and weight.
+      */
       public double calculateBMI()
          {
             double BMI = 0;
@@ -136,7 +184,11 @@ public class Policy
             
             return BMI;
          }
-         
+
+      /**
+         The calculatePrice method returns the value of the total_cost variable.
+         @return The calculation of the total cost based off user input.
+      */
      public double calculatePrice()
          {
             double base_fee       = 600.00;
