34710dbe8498b418bb50f949fba17707aca19012
kadgrif <kgriff60@email.cpcc.edu>
2025-04-12 22:18:19 -0400

    Added code to implicitly call toString method and output information about Policy and PolicyHolders

--- Policy.java
+++ Policy.java
@@ -1,42 +1,32 @@
 public class Policy
    {
+      private final int AGE_LIMIT = 50;       // Age limit to help calculate a fee for any age over this number
+      private final int BMI_LIMIT = 35;       // BMI limit to help calculate a fee for anyone over it
+        
+      private String providerName;        // Insurance Provider Name   
+      private int policyNumber;           // Insurance Policy Number
+      private PolicyHolder policyholder;  // Creating an object or instance of the policy holder class
       
-      private String providerName;  // Insurance Provider Name
-      private String firstName;     // First name of policyholder
-      private String lastName;      // Last name of policyholder
-      private String smokingStatus; // Are you a smoker or non-smoker? 
+      // Constructor
       
-      private int policyNumber;  // Insurance Policy Number
-      private int age;              // Age of the policyholder
-      private double height;           // Height of the policyholder
-      private double weight;           // Weight of the policyholder
-      
+      /**
+         Default Constructor
+      */
       public Policy()
          {
-            
-            providerName  = "";
-            firstName     = "";
-            lastName      = "";
-            smokingStatus = "";
-            
+            providerName  = "";         
             policyNumber  = 0;
-            age           = 0;
-            height        = 0;
-            weight        = 0;
+            policyholder = new PolicyHolder();
          }
-         
-      public Policy(int pPolicyNumber, String pProviderName, String pFirstName,
-                    String pLastName, String pSmokingStatus, int pAge, double pHeight, double pWeight)
+      
+      /**
+         Constructor that accepts agruments
+      */   
+      public Policy(int pPolicyNumber, String pProviderName, PolicyHolder pObject)
          {
             policyNumber  = pPolicyNumber;
             providerName  = pProviderName;
-            firstName    = pFirstName;
-            lastName      = pLastName;
-            smokingStatus = pSmokingStatus;
-            
-            age           = pAge;
-            height        = pHeight;
-            weight        = pWeight;
+            policyholder  = new PolicyHolder(pObject);
          }
 
       /**
@@ -74,115 +64,13 @@ public class Policy
          }
 
       /**
-         The setFirstName method mutates the value of the firstName variable.
-      */
-      public void setFirstName(String pFirstName)
-         {
-            firstName = pFirstName;
-         } 
-
-      /**
-         The getFirstName method returns the value of the firstName variable.
-         @return The user's first name.
-      */
-      public String getFirstName()
-         {
-            return firstName;
-         }
-
-      /**
-         The setLastName method mutates the value of the lastName variable.
-      */
-      public void setLastName(String pLastName)
-         {
-            lastName = pLastName;
-         } 
-
-      /**
-         The getLastName method returns the value of the lastName variable.
-         @return The user's last name.
-      */
-      public String getLastName()
-         {
-            return lastName;
-         }
-      
-      /**
-         The setSmokingStatus method mutates the value of the smokingStatus variable.
-      */   
-      public void setSmokingStatus(String pSmokingStatus)
-         {
-            smokingStatus = pSmokingStatus;
-         }   
-
-      /**
-         The getSmokingStatus method returns the value of the smokingStatus variable.
-         @return The smoking status of the user.
-      */
-      public String getSmokingStatus()
-         {
-            return smokingStatus;
-         }
-
-      /**
-         The setAge method mutates the value of the age variable.
-      */
-      public void setAge(int pAge)
-         {
-            age = pAge;
-         }    
-
-      /**
-         The getAge method returns the value of the age variable.
-         @return The age of the user.
-      */
-      public int getAge()
-         {
-            return age;
-         }  
-
-      /**
-         The setHeight method mutates the value of the height variable.
-      */
-      public void setHeight(double pHeight)
-         {
-            height = pHeight;
-         }     
-
-      /**
-         The getHeight method return the value of the height variable.
-         @return The height of the user.
-      */
-      public double getHeight()
-         {
-            return height;
-         }
-
-      /**
-         The setWeight method mutates the value of the weight variable.
-      */
-      public void setWeight(double pWeight)
-         {
-            weight = pWeight;
-         } 
-
-      /**
-         The getWeight method returns the value of the weight variable.
-         @return The weight of the user.
-      */
-      public double getWeight()
-         {
-            return weight;
-         }
-
-      /**
          The calculateBMI method returns the value of the BMI variable.
          @return The calculation of Body Mass Index based of user age and weight.
       */
       public double calculateBMI()
          {
             double BMI = 0;
-            BMI = (weight * 703)/Math.pow(height,2);
+            BMI = (policyholder.getWeight() * 703)/Math.pow(policyholder.getHeight(),2);
             
             return BMI;
          }
@@ -192,7 +80,7 @@ public class Policy
          @return The calculation of the total cost based off user input.
       */
 
-     public double calculatePrice()
+      public double calculatePrice()
          {
             double base_fee       = 600.00;
             double age_fee = 75.00;
@@ -200,11 +88,11 @@ public class Policy
             double BMI_fee        = (calculateBMI() - 35) * 20;
             double total_cost     = 0;
             
-            if(age > 50)
+            if(policyholder.getAge() > AGE_LIMIT)
                {
-                  if(smokingStatus.equalsIgnoreCase("smoker"))
+                  if(policyholder.getSmokingStatus().equalsIgnoreCase("smoker"))
                      {
-                        if(calculateBMI() > 35)
+                        if(calculateBMI() > BMI_LIMIT)
                            {
                               total_cost = base_fee + age_fee + smoker_fee + BMI_fee; 
                            }
@@ -221,9 +109,9 @@ public class Policy
                
             else
                {
-                  if(smokingStatus.equalsIgnoreCase("smoker"))
+                  if(policyholder.getSmokingStatus().equalsIgnoreCase("smoker"))
                      {
-                        if(calculateBMI() > 35)
+                        if(calculateBMI() > BMI_LIMIT)
                            {
                               total_cost = base_fee + smoker_fee + BMI_fee; 
                            }
@@ -234,7 +122,7 @@ public class Policy
                      }
                   else
                      {
-                        if(calculateBMI() > 35)
+                        if(calculateBMI() > BMI_LIMIT)
                            {
                               total_cost = base_fee + BMI_fee; 
                            }
@@ -247,5 +135,10 @@ public class Policy
                }
                
            return total_cost;
-         }              
+         } 
+     
+      public void ToString()
+         {
+            System.out.printf("%n%nPolicy Number: %d %nProvider Name: %s %n%s %nPolicyholder's BMI: %,.2f %nPolicy Price: $%,.2f", policyNumber, providerName, policyholder.ToString(), calculateBMI(), calculatePrice());  
+         }                 
    }
